795c9a072d9d92c83f12fff612a885db853de4c7
ndespouy <ndespouy@gmail.com>
2023-07-25 14:11:50 +0400

    i18n - entity folder in progress

--- datahub-web-react/src/app/entity/shared/tabs/ML/MlPrimaryKeyFeatureTableTab.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/ML/MlPrimaryKeyFeatureTableTab.tsx
@@ -4,14 +4,16 @@ import { EntityType } from '../../../../../types.generated';
 import { EntityList } from '../Entity/components/EntityList';
 import { useEntityRegistry } from '../../../../useEntityRegistry';
 import { GetMlPrimaryKeyQuery } from '../../../../../graphql/mlPrimaryKey.generated';
+import { useTranslation } from 'react-i18next';
 
 export const FeatureTableTab = () => {
     const entity = useBaseEntity() as GetMlPrimaryKeyQuery;
     const entityRegistry = useEntityRegistry();
+    const { t } = useTranslation();
 
     const feature = entity && entity.mlPrimaryKey;
     const featureTables = feature?.featureTables?.relationships.map((relationship) => relationship.entity);
 
-    const title = `Part of ${entityRegistry.getEntityName(EntityType.MlfeatureTable)}`;
+    const title = t('common.partOfWithName', { name: entityRegistry.getEntityNameTrans(EntityType.MlfeatureTable, t)});
     return <EntityList title={title} type={EntityType.MlfeatureTable} entities={featureTables || []} />;
 };

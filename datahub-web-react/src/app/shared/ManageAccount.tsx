d4d8351e14c198d0955790e75ee4a116c34e9dd1
ndespouy <ndespouy@gmail.com>
2023-07-25 10:27:07 +0400

    i18n - Clean

--- datahub-web-react/src/app/shared/ManageAccount.tsx
+++ datahub-web-react/src/app/shared/ManageAccount.tsx
@@ -64,7 +64,6 @@ export const ManageAccount = ({ urn: _urn, pictureLink: _pictureLink, name }: Pr
         userContext.updateLocalState({ selectedViewUrn: undefined });
     };
     const version = config?.appVersion;
-    // TODO ndespouy traduction depuis le thème
     const menu = (
         <Menu style={{ width: '120px' }}>
             {version && (

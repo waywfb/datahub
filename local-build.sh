825afb15ea79de064e3a6c1e76733d82f5feb7dc
Jean-Michel <jean-michel.grondin@epitech.eu>
2023-09-05 14:44:51 +0400

    add a local-build script to we use to build the app at this state

---
+++ local-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# In case you have multiple user building on the same machine, you can run this two line to fix the permission issue we encountered.
+# sudo chown $USER:$USER /tmp/datahub/logs/*-consumer-job.log
+# sudo chown $USER:$USER /tmp/datahub/logs/datahub-upgrade/upgrade*
+
+# expor this env variable to fix a build issue we encountered when building the web-frontend-react and the airflow-plugin
+# export NODE_OPTIONS=--max_old_space_size=4096
+
+./gradlew :metadata-io:compileTestJava
+
+./gradlew :datahub-web-react:yarnTest
+
+./gradlew build \
+    -x :metadata-io:checkstyleTest \
+    -x :metadata-ingestion:buildWheel \
+    -x :metadata-io:test \
+    -x :metadata-ingestion:testQuick \
+    -x :datahub-web-react:yarnTest
